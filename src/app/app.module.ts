42ee802b4dca3410fe0100ee5f90b4663b40c6c6
Oneonjr <oneonjunior@outlook.com>
2023-04-11 21:34:18 -0300

    Criando os Componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,25 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { CabecalhoComponent } from './componentes/cabecalho/cabecalho.component';
+import { PackagesComponent } from './componentes/packages/packages.component';
+import { PackageDetailsComponent } from './componentes/packages/package-details/package-details.component';
+import { PackageListComponent } from './componentes/packages/package-list/package-list.component';
+import { PackageItemComponent } from './componentes/packages/package-list/package-item/package-item.component';
+import { FoodListComponent } from './componentes/food-list/food-list.component';
+import { FoodEditComponent } from './componentes/food-list/food-edit/food-edit.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CabecalhoComponent,
+    PackagesComponent,
+    PackageDetailsComponent,
+    PackageListComponent,
+    PackageItemComponent,
+    FoodListComponent,
+    FoodEditComponent,
   ],
   imports: [
     BrowserModule
