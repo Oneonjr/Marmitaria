fc714b86f9e0a97826b7d0218b9a08430e58229b
Oneonjr <oneonjunior@outlook.com>
2023-04-13 20:03:30 -0300

    Adicionado primeira marmita

--- src/app/componentes/packages/package-list/package-list.component.ts
+++ src/app/componentes/packages/package-list/package-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Package } from './package.model';
 
 @Component({
   selector: 'app-package-list',
@@ -6,5 +7,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./package-list.component.css']
 })
 export class PackageListComponent {
+  packages: Package[] = [
+    new Package('Churrasco', 'Churrasco de Picanha', 'https://dd7gu835n6agp.cloudfront.net/wp-content/uploads/2021/08/picanha-na-grelha-889x500.jpg' ),
+    new Package('Churrasco', 'Churrasco de Picanha', 'https://dd7gu835n6agp.cloudfront.net/wp-content/uploads/2021/08/picanha-na-grelha-889x500.jpg' )
+
+  ];
 
 }
